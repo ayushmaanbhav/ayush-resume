d51b55422db0545af55a6da1985bd45823516584
ayush <ayushmaanbhav1008@gmail.com>
2025-08-25 22:58:07 +0530

    initial commit

--- resume-app/src/components/resume/Hobbies.js
+++ resume-app/src/components/resume/Hobbies.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
 const Hobbies = ({ hobbies }) => (
-  <section id="hobbies" className="resume-section">
-    <h2>Other Skills and Hobbies</h2>
-    <div className="skills-list">
-      <p dangerouslySetInnerHTML={{ __html: hobbies }} />
-    </div>
-  </section>
+    <section id="hobbies" className="resume-section">
+        <h2>Other Skills and Hobbies</h2>
+        <div className="hobbies-list">
+            <div className="hobby-tags-container">
+                {hobbies.map((hobby, index) => (
+                    <span key={index} className="hobby-tag">
+                        <span className="hobby-icon">{hobby.icon}</span>
+                        {hobby.name}
+                    </span>
+                ))}
+            </div>
+        </div>
+    </section>
 );
 
 export default Hobbies;
